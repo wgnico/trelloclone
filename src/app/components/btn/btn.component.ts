681c4a06dc78eafe57cb68062bca369dc70391bf
wgnico <nicoburgos1989@gmail.com>
2025-04-22 17:12:50 -0300

    se agrega archivos

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn.component.html'
+})
+export class BtnComponent {
+
+  @Input() typeBtn: 'button' | 'submit' | 'reset' = 'button';
+  @Input() color: 'green' | 'blue' = 'blue';
+
+  get colors(){
+    return {
+      'bg-green-700': true,
+      'hover:bg-green-800': true,
+      'focus:ring-green-300': true,
+      'bg-blue-700': true,
+      'hover:bg-blue-800': true,
+      'focus:ring-blue-300': true,
+    };
+  }
+
+}
