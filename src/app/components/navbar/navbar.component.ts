7becf30ff5ce997c9e2a0be5ff2af7c829a3b57b
wgnico <nicoburgos1989@gmail.com>
2025-11-05 13:33:46 -0300

    se agrega overlays al navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { BtnComponent } from '../btn/btn.component';
 import {OverlayModule} from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { faBell, faInfoCircle, faClose } from '@fortawesome/free-solid-svg-icons';
+
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BtnComponent, OverlayModule],
+  imports: [BtnComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
+  faClose = faClose;
+
   isOpen = false;
 }
