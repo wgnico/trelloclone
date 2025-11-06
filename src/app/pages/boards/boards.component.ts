6bcee134126af3ca69f4cad1e6b324bef93befcf
wgnico <nicoburgos1989@gmail.com>
2025-11-06 08:31:50 -0300

    se agrega accordion a el boards

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {faTrello} from '@fortawesome/free-brands-svg-icons'
-import {faBox, faWaveSquare, faClock} from '@fortawesome/free-solid-svg-icons'
+import {faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorderAll, faUsers, faGear} from '@fortawesome/free-solid-svg-icons'
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-boards',
-    imports: [NavbarComponent,FontAwesomeModule],
+    imports: [NavbarComponent,FontAwesomeModule, CdkAccordionModule, CommonModule],
     templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
@@ -14,5 +16,48 @@ export class BoardsComponent {
     faBox = faBox;
     faWaveSquare = faWaveSquare;
     faClock = faClock;
-    
+    faAngleUp = faAngleUp;
+    faAngleDown = faAngleDown;
+    faHeart = faHeart;
+    faBorderAll = faBorderAll;
+    faUsers = faUsers;
+    faGear = faGear;
+
+    items = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Sub Item 1.1'
+        },
+        {
+          label: 'Sub Item 1.2'
+        }
+      ]
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Sub Item 2.1'
+        },
+      ]
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Sub Item 3.1'
+        },
+        {
+          label: 'Sub Item 3.2'
+        },
+        {
+          label: 'Sub Item 3.3'
+        },
+      ]
+    }
+  ];
+
+
 }
