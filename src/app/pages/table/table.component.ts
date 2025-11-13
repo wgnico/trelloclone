179d5da71ec134dc75158a949db407b9e3951ffd
wgnico <nicoburgos1989@gmail.com>
2025-11-13 09:41:57 -0300

    se agrega logica

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,27 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {CdkTableModule} from '@angular/cdk/table';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Product } from '../../models/product.model';
+import { DataSourceProduct } from './data-source';
+import { BtnComponent } from '../../components/btn/btn.component';
 
 @Component({
   selector: 'app-table',
-  imports: [CdkTableModule,NavbarComponent, HttpClientModule, ],
+  imports: [CdkTableModule,NavbarComponent, HttpClientModule, BtnComponent],
   templateUrl: './table.component.html',
 })
-export class TableComponent {
+export class TableComponent implements OnInit{  
+  columns: string[] = ['#No', 'Name', 'price', 'cover', 'actions'];
+  total= 0;
+  dataSource = new DataSourceProduct();
   products: Product[] = [];
-  columns: string [] = ['id', 'title', 'price', 'cover']
 
   constructor(
     private http: HttpClient
   ){}
 
   ngOnInit(): void{
-    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products').subscribe(data=> {
-      this.products = data;
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe(data=> {
+      this.dataSource.init(data);
+      this.total = this.dataSource.getTotal();
     })
 
+  }
+
+  update(product: Product) {
+    this.dataSource.update(product.id, { price: 20 });
   }
 
 }
